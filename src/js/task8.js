a88809b342ff08c49c3c54c9702ea80f34594055
GebrinAndriy <gebrinandriy@gmail.com>
2022-01-20 19:07:45 +0200

    Task8.update

--- src/js/task8.js
+++ src/js/task8.js
@@ -2,22 +2,20 @@ const form = document.querySelector('.login-form');
 
 const onFormSubmit = (event) => {
     event.preventDefault();
-    const formElements = event.currentTarget.elements;
-    const formEmail = formElements.email.value;
-    const formPassword = formElements.password.value;
+    const formEmail = event.currentTarget.elements.email.value;
+    const formPassword = event.currentTarget.elements.password.value;
     const User = {
         email: formEmail,
-        password: formPassword,
-    }
+        password: formPassword
+    };
     if (User.email === "" || User.password === "") {
-        return window.alert("You must fill in all the fields");
+        return alert("You must fill in all the fields");
     }
     else {
         console.log(`Email: ${User.email}, password: ${User.password}`);
         event.currentTarget.reset();
     }
     
-}
+};
 
 form.AddEventListener('submit', onFormSubmit);
-
